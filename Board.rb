a28c0e58874ec42ea404be067d1be9f7576224a4
sophiaquach16 <sophiaquach16@gmail.com>
2017-06-04 22:45:14 -0400

    Put @ in front of fields

--- Board.rb
+++ Board.rb
@@ -5,22 +5,22 @@ class Board
   def initialize(noRows, noColumns)
     @noRows = noRows
     @noColumns = noColumns
-    
-    for i in 0..(noRows-1)
-      board[i] = Array.new(noColumns) # for each row there are 7 slots vertically
+
+    for i in 0..(@noRows-1)
+      @board[i] = Array.new(@noColumns) # for each row there are 7 slots vertically
     end
-    for i in 0..(noRows-1)
-      for j in 0..(noColumns-1)
-        board[i][j] = "X"
+    for i in 0..(@noRows-1)
+      for j in 0..(@noColumns-1)
+        @board[i][j] = "X"
       end
     end
   end
 
-  def add(board, colour, column)
-    for i in 0..(noRows-1)
-      w = (i - (noRows-1)) * (-1)
-      if (board[w][column] == "X") then
-        board[w][column] = colour
+  def add(colour, column)
+    for i in 0..(@noRows-1)
+      w = (i - (@noRows-1)) * (-1)
+      if (@board[w][column] == "X") then
+        @board[w][column] = colour
         break
       else
         print "Column is full."
@@ -28,16 +28,17 @@ class Board
     end
   end
 
-  def print(board)
-    for i in 0..(noRows-1)
-      for j in 0..(noColumns-1)
-        print board[i][j]
+  def print()
+    for i in 0..(@noRows-1)
+      for j in 0..(@noColumns-1)
+        print @board[i][j]
         print " "
       end
       puts
     end
   end
 
-  def getWinner(board)
+  def getWinner()
+  end
 
 end
---
+++ testing.rb
@@ -0,0 +1,2 @@
+board = Board.new(5,5)
+board.print(board)
