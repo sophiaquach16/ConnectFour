b1083cd68c8d4c64ee0fe28daed18f34fd1e49e8
sophiaquach16 <sophiaquach16@gmail.com>
2017-06-04 22:37:03 -0400

    created a Board.rb initialize method

--- Board.rb
+++ Board.rb
@@ -1,22 +1,24 @@
 class Board
-  # creation of a new board ----------------------------------------------------
-  
-  for i in 0..5
-    board[i] = Array.new(7) # for each row there are 7 slots vertically
-  end
-
-  for i in 0..5
-    for j in 0..6
-      board[i][j] = "X"
-    end
-  end
 
   # methods below --------------------------------------------------------------
 
+  def initialize(noRows, noColumns)
+    @noRows = noRows
+    @noColumns = noColumns
+    
+    for i in 0..(noRows-1)
+      board[i] = Array.new(noColumns) # for each row there are 7 slots vertically
+    end
+    for i in 0..(noRows-1)
+      for j in 0..(noColumns-1)
+        board[i][j] = "X"
+      end
+    end
+  end
 
   def add(board, colour, column)
-    for i in 0..5
-      w = (i - 5) * (-1)
+    for i in 0..(noRows-1)
+      w = (i - (noRows-1)) * (-1)
       if (board[w][column] == "X") then
         board[w][column] = colour
         break
@@ -27,8 +29,8 @@ class Board
   end
 
   def print(board)
-    for i in 0..5
-      for j in 0..6
+    for i in 0..(noRows-1)
+      for j in 0..(noColumns-1)
         print board[i][j]
         print " "
       end
