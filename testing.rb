211f3709cd1b9e56d1c1795407ec493058f1b886
sophiaquach16 <sophiaquach16@gmail.com>
2017-06-10 15:34:01 -0400

    Added unit test for finding winner

--- testing.rb
+++ testing.rb
@@ -1,5 +1,83 @@
 require_relative 'Board'
 board = Board.new(5,5)
+
+def testDiagonalWinner()
+  board1 = Board.new(5,5)
+  board1.add("T",0)
+  board1.add("P",1)
+  board1.add("T",1)
+  board1.add("P",2)
+  board1.add("P",2)
+  board1.add("T",2)
+  board1.add("P",3)
+  board1.add("P",3)
+  board1.add("P",3)
+  board1.add("T",3)
+
+  isOrIsNot = board1.getWinner("T")
+
+  if (isOrIsNot != "T")
+    puts "Could not find a winner.\n"
+  elsif
+    puts "Found diagonal winner.\n"
+  end
+
+end
+
+testDiagonalWinner()
+
+def testHorizontalWinner()
+  board1 = Board.new(5,5)
+  board1.add("T",0)
+  board1.add("T",1)
+  board1.add("P",1)
+  board1.add("T",2)
+  board1.add("P",2)
+  board1.add("P",2)
+  board1.add("T",3)
+  board1.add("T",3)
+  board1.add("P",3)
+  board1.add("P",3)
+
+  isOrIsNot = board1.getWinner("T")
+
+  if (isOrIsNot != "T")
+    puts "Could not find a winner.\n"
+  elsif
+    puts "Found horizontal winner.\n"
+  end
+
+end
+
+testHorizontalWinner()
+
+def testVerticalWinner()
+  board1 = Board.new(5,5)
+  board1.add("T",0)
+  board1.add("T",1)
+  board1.add("P",1)
+  board1.add("P",2)
+  board1.add("P",2)
+  board1.add("P",2)
+  board1.add("T",3)
+  board1.add("T",3)
+  board1.add("T",3)
+  board1.add("T",3)
+
+  isOrIsNot = board1.getWinner("T")
+
+  if (isOrIsNot != "T")
+    puts "Could not find a winner.\n"
+  elsif
+    puts "Found vertical winner.\n"
+  end
+
+end
+
+testVerticalWinner()
+
+=begin
+
 board.add("B",1)
 board.add("B",1)
 board.add("A",1)
@@ -24,3 +102,5 @@ board.print
 #board.getRightDiagonalWinner("B")
 
 board.getWinner("B")
+
+=end
